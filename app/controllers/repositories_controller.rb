6f0bff9c3259ed468adfd5931f3ae1a734bec84b
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-10 15:15:47 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,6 +7,11 @@ class RepositoriesController < ApplicationController
     end
 
     @user_data = JSON.parse(user.body)
+
+    repos = Faraday.get 'https://api.github.com/user/repos' do |req|
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
   end
 
 end
