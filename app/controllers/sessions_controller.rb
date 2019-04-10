f87ab11178e8e1096f94a1f40a4bff5a153fc1ad
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-10 15:10:33 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    @resp = Faraday.get("") do |req|
+    @resp = Faraday.post("") do |req|
       req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
       req.headers['Accept'] = 'application/json'
     end
