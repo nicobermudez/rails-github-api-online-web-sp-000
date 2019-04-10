688354ea0a798968e49d04f9ed837397a1263a36
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-10 15:18:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user
 
-  before_action :authenticate_user
-
   private
 
   def authenticate_user
