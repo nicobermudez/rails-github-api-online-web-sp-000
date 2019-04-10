a56758cd6575703ca259a1de04cc237467c4f08e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-10 15:03:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   def authenticate_user
     client_id = ENV['GITHUB_ID']
     redirect_uri = CGI.escape("http://localhost:3000/auth")
-    foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+    github_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
     redirect_to foursquare_url unless logged_in?
   end
 
